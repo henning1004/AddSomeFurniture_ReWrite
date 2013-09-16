888aa8f735740789bdb97505c01f9548ab984a25
Henning <henning_janssen@ewetel.net>
2013-09-16 18:37:05 +0200

    baseblock added
    added cobblestone wall as baseblock for flowerboxes

--- src/me/henning1004/addsomefurniture/custom/FlowerBox.java
+++ src/me/henning1004/addsomefurniture/custom/FlowerBox.java
@@ -4,6 +4,7 @@ import me.henning1004.addsomefurniture.Main;
 import me.henning1004.addsomefurniture.design.FlowerBoxDesign;
 import me.henning1004.addsomefurniture.register.RegisterTextures;
 
+import org.bukkit.Material;
 import org.getspout.spoutapi.block.design.GenericCubeBlockDesign;
 import org.getspout.spoutapi.material.block.GenericCubeCustomBlock;
 
@@ -11,8 +12,9 @@ public class FlowerBox extends GenericCubeCustomBlock {
 	@SuppressWarnings("unused")
 	private Main plugin;
 	
+	@SuppressWarnings("deprecation")
 	public FlowerBox(Main plugin,String name, int[] textureids, String dir) {
-		super(plugin, name, false, new GenericCubeBlockDesign(plugin, RegisterTextures.texOther, textureids));
+		super(plugin, name, Material.COBBLE_WALL.getId(), new GenericCubeBlockDesign(plugin, RegisterTextures.texOther, textureids));
 		this.setBlockDesign(new FlowerBoxDesign(plugin, textureids, dir));
 		this.setHardness(3.5F);
 		this.plugin = plugin;
